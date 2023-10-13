dd03a0fdafa64244b4d78bb7fd877462952223c9
Maxwell <lubalecliff@gmail.com>
2023-10-13 08:55:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+// Remove the <main> element with id 'main'
+let mainElement = document.querySelector('main');
+mainElement.remove();
+// Create a new <h1> element with id 'victory'
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+// Set the text content of the new <h1> element to "YOUR-NAME is the champion"
+newHeader.textContent = "Maxcliff is the champion";
+
